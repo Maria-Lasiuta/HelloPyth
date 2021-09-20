f0a45104c732a3d487ebd6c7e1f844957eb8526b
OlenaKalitsinska <90180782+OlenaKalitsinska@users.noreply.github.com>
2021-09-20 23:44:02 +0300

    Update Ex8(lab2).py

--- Ex8(lab2).py
+++ Ex8(lab2).py
@@ -1,10 +1,12 @@
-a=['d','cccc','bb','aaa']
+list1=['d','cccc','bb','aaa']
 
-def mysort(mylist=[],numflag=False):
-    if numflag==False:
-        a.sort(key=lambda x: x[0])
+def mysort(mylist = [],numflag = False):
+    if numflag == False:#
+        list1.sort(key = lambda x: x[0])
     else:
-        a.sort(key=len)
-    return print(a)
-mysort(a)
-mysort(a,True)
+        list1.sort(key = len)
+    return print(list1)
+
+mysort(list1)
+mysort(list1, True)
+print('DONE')

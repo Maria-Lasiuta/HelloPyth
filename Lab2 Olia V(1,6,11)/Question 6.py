14e76cfe8486798a08f5201e1286d616179a4d3d
OlhaVeselovska <90180548+OlhaVeselovska@users.noreply.github.com>
2021-09-20 21:36:35 +0300

    Update Question 6.py

--- Lab2 Olia V(1,6,11)/Question 6.py
+++ Lab2 Olia V(1,6,11)/Question 6.py
@@ -7,28 +7,31 @@ Print the list in reverse order.
 Print Yes if the list contains a 5 and No otherwise.
 Print the number of fives in the list.
 Remove the first and last items from the list, sort the remaining items, and print the result.
-Print how many integers in the list are less than 5.'''
-def lists(list_of_integer):
-          print(len(list_of_integer))
-          print(list_of_integer[-1])
-          list_of_integer.reverse()
-          print(list_of_integer)
-          if '5' in list_of_integer:
-              print('Yes')
-          else:
-              print('No')
-          count = 0
-          for i in list_of_integer:
-              if i == '5':
-                  count+=1
-          print(count)
-          list_of_integer[1:-1]
-          list_of_integer.sort()
-          print(list_of_integer)
-          number=0
-          for i in list_of_integer:
-              if i < '5':
-                  number+=1
-          print(number)
-list_of_integer  = list(input('Введіть список з цілих чисел: '))
-lists(list_of_integer)           
+Print how many integers in the list are less than 5.'''      
+def lists(list_of_integer): 
+          print(len(list_of_integer)) 
+          if '5' in list_of_integer: 
+              print('Yes') 
+          else: 
+              print('No') 
+          number=0 
+          count = 0 
+          for i in list_of_integer: 
+              if i == '5': 
+                  count+=1 
+          print(f'{count}-кількість 5-ірок в списку') 
+          for i in list_of_integer: 
+              if i < '5': 
+                  number+=1 
+          print(f'{number}-кількість чисел менших 5-ти') 
+          print(list_of_integer[-1], '-останній елемент') 
+          list_of_integer.reverse() 
+          print(f'{list_of_integer}-список перевернутий') 
+          list_of_integer.reverse() 
+          list_of_integer.pop(0) 
+          list_of_integer.pop(-1) 
+          list_of_integer.sort() 
+          print(f'{list_of_integer}- посортований список без першого і останнього елементу') 
+           
+list_of_integer  = list(input('Введіть список з цілих чисел: ')) 
+lists(list_of_integer)

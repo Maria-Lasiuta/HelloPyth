40c7508d5290401c7dd51797a20da7b6a1fce18a
ChristinaDanylova <90180543+ChristinaDanylova@users.noreply.github.com>
2021-09-21 23:05:12 +0300

    Add files via upload

---
+++ ex_7.py
@@ -0,0 +1,8 @@
+my_str = str(input('Введіть список: '))
+my_list = list(my_str)
+temp_list = []
+for i in my_list:
+    if i not in temp_list:
+        temp_list.append(i)
+my_list = temp_list
+print(my_list)

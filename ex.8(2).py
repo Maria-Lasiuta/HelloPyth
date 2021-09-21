37d02664ff7dd7c6d67f29673d0c91232713c628
OlenaKalitsinska <90180782+OlenaKalitsinska@users.noreply.github.com>
2021-09-21 12:30:36 +0300

    Update ex.8(2).py

--- ex.8(2).py
+++ ex.8(2).py
@@ -2,10 +2,10 @@ list1=['d','cccc','bb','aaa']
 
 def mysort(mylist = [],numflag = False):
     if numflag == False:
-        list1.sort(key = lambda x: x[0])
+        mylist.sort(key = lambda x: x[0])
     else:
-        list1.sort(key = len)
-    return print(list1)
+        mylist.sort(key = len)
+    return print(mylist)
 
 mysort(list1)
 mysort(list1, True)

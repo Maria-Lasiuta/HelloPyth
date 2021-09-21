86f3f5d4460dd5d08181c70941518a196d675b9b
ChristinaDanylova <90180543+ChristinaDanylova@users.noreply.github.com>
2021-09-21 23:00:38 +0300

    Delete ex_7.py

--- ex_7.py
+++
@@ -1,8 +0,0 @@
-my_str = str(input('Введіть список :'))
-my_list = list(my_str)
-temp_list = []
-for i in my_list:
-    if i not in temp_list:
-        temp_list.append(i)
-my_list = temp_list
-print(my_list)
